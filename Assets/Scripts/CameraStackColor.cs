77c2cfbdc0552b1cf6633bbca05537e676ba1dfa
=blacksw <blacksw1102@gmail.com>
2022-04-29 16:45:13 +0900

    Player 측면이동 기능 추가

--- Assets/Scripts/CameraStackColor.cs
+++ Assets/Scripts/CameraStackColor.cs
@@ -4,19 +4,16 @@ public class CameraStackColor : MonoBehaviour
 {
     [SerializeField] Transform target;
     float deltaZ;
-    int count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         deltaZ = transform.position.z - target.position.z;
-        Debug.Log(deltaZ);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y, target.position.z + deltaZ);
-        Debug.Log(count++);
     }
 }
--- Assets/Scripts/PlayerControllerStackColor.cs
+++ Assets/Scripts/PlayerControllerStackColor.cs
@@ -7,6 +7,7 @@ public class PlayerControllerStackColor : MonoBehaviour
 
     [SerializeField] bool isPlaying;
     [SerializeField] float forwardSpeed;
+    [SerializeField] float sideLerpSpeed;
     Rigidbody myRB;
 
     // Start is called before the first frame update
@@ -23,6 +24,15 @@ public class PlayerControllerStackColor : MonoBehaviour
         {
             MoveForward();
         }
+
+        if(Input.GetMouseButton(0))
+        {
+            if(isPlaying == false)
+            {
+                isPlaying = true;
+            }
+            MoveSideways();
+        }
     }
 
     void SetColor(Color colorIn)
@@ -38,4 +48,15 @@ public class PlayerControllerStackColor : MonoBehaviour
     {
         myRB.velocity = Vector3.forward * forwardSpeed;
     }
+
+    void MoveSideways()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 100))
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(hit.point.x, transform.position.y, transform.position.z), sideLerpSpeed * Time.deltaTime);
+        }
+    }
+
 }
